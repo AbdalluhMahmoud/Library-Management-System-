4f5918fa83b91c6c67012c47de10e2c721f1b462
AbdalluhMahmoud58 <a01024004113@gmail.com>
2025-11-29 10:53:32 +0200

    Added project classes

---
+++ libraryMangementSystem/src/main/java/com/mycompany/librarymangementsystem/LibraryMangementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.librarymangementsystem;
+
+/**
+ *
+ * @author 11
+ */
+public class LibraryMangementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
