8eb5a088600337cd8e05cd1588b1e7c689ba2c86
Habiba Hassan <habiba2006.hasan@icloud.com>
2025-12-11 17:45:37 +0200

    Update Librarien.java

--- LibraryMangementSystem/librarymangementsystem/Librarien.java
+++ LibraryMangementSystem/librarymangementsystem/Librarien.java
@@ -7,6 +7,33 @@
  *
  * @author 11
  */
-public class Librarien {
+package com.mycompany.book_store;
+
+public class Librarian extends User{
+  private String employee_ID;
+
+    public Librarian() {
+    }
+
+    public Librarian(String employee_ID, String id, String name, String email, String password) {
+        super(id, name, email, password);
+        this.employee_ID = employee_ID;
+    }
+
+    public String getEmployee_ID() {
+        return employee_ID;
+    }
+
+    public void setEmployee_ID(String employee_ID) {
+        this.employee_ID = employee_ID;
+    }
     
+@Override
+public String toString() {
+    return "Librarian{" + super.toString() + ", employee_ID=" + employee_ID + '}';
 }
+
+
+    
+  
+    }
