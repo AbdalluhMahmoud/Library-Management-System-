683fc81131782924e0dcbfa6690a8ca47e176615
Habiba Hassan <habiba2006.hasan@icloud.com>
2025-12-11 17:44:00 +0200

    Update Main.java

--- LibraryMangementSystem/librarymangementsystem/Main.java
+++ LibraryMangementSystem/librarymangementsystem/Main.java
@@ -7,6 +7,17 @@
  *
  * @author 11
  */
-public class Main {
+package com.mycompany.book_store;
+
+
+public class Book_Store {
+
+    public static void main(String[] args) {
+           Book b1 = new Book("B001", "Author1", "Java Basics", "Pub1", 2023, 5, "Programming");
+Patron p1 = new Patron("0123456789", "Cairo", "P001", "Ali", "ali@gmail.com", "pass123");
+System.out.println(p1);
+
+    }
     
+
 }
