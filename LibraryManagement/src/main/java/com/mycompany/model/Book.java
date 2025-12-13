a2daba6545231a2fb08c858aa52b3cb45343464a
Habiba Hassan <habiba2006.hasan@icloud.com>
2025-12-13 22:07:54 +0200

    Update Book.java

--- LibraryManagement/src/main/java/com/mycompany/model/Book.java
+++ LibraryManagement/src/main/java/com/mycompany/model/Book.java
@@ -86,4 +86,72 @@ public class Book {
     
 
     
+}public class Book {
+
+    private String isbn;
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    public Book() {}
+
+    public Book(String isbn, String title, String author, int year) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = true; // default when adding a new book
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "ISBN='" + isbn + '\'' +
+                ", Title='" + title + '\'' +
+                ", Author='" + author + '\'' +
+                ", Year=" + year +
+                ", Available=" + isAvailable +
+                '}';
+    }
 }
