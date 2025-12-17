3b7b2662ce538c92ef685a971a620cc16cd84ba0
JumanaEmad <gomanaemad833@gmail.com>
2025-12-17 17:08:24 +0200

    add FileHelper class

---
+++ LibraryManagement/src/main/java/com/mycompany/TextFiles/TextFiles/FileHelper.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class FileHelper {
+    public static List<String> readFile(String fileName) throws IOException {
+        List<String> lines = new ArrayList<>();
+        File file = new File(fileName);
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        try(Scanner sc = new Scanner(file)){
+            while(sc.hasNextLine()){
+                lines.add(sc.nextLine());
+            }
+        }
+        return lines;
+    }
+    
+    public static void writeFile(String fileName,List<String> lines)throws IOException{
+        try(PrintWriter pw = new PrintWriter(new FileWriter(fileName))){
+            for(String line : lines){
+                pw.println(line);
+            }
+        }
+    }
+    
+    public static void appendLine(String fileName , String line)throws IOException{
+        try(PrintWriter pw = new PrintWriter(new FileWriter(fileName, true))){
+            pw.println(line);
+        }
+    }
+ 
+}
